7dee97cc940c22f6d43ecc82d690d6eea7200a96
spences10 <spences10apps@gmail.com>
2017-06-06 18:33:30 +0100

    Multiple Query Promises with async await 🙃

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -83,3 +83,13 @@ exports.getStoreBySlug = async (req, res, next) => {
   if(!store) return next()
   res.render('store', { store, title: store.name })
 }
+
+exports.getStoreByTag = async (req, res) => {
+  const tag = req.params.tag
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList()
+  const storesPromise = Store.find({ tags: tagQuery })
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise])
+  res.render('tag', { tags, title: 'Tags', tag, stores })
+}
+
--- models/Store.js
+++ models/Store.js
@@ -51,4 +51,12 @@ storeSchema.pre('save', async function (next) {
   // TODO make mor resiliant for unique slugs
 })
 
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ])
+}
+
 module.exports = mongoose.model('Store', storeSchema)
