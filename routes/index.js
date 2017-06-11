0b94fa7112aec7971d8c0f8a3c948fc7da15ddc6
spences10 <spences10apps@gmail.com>
2017-06-11 13:41:33 +0100

    Creating User Accounts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // so requset is the information and
@@ -26,5 +27,13 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 
 router.get('/tags', catchErrors(storeController.getStoreByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag))
+
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+
+// 1. validate data
+// 2. register user
+// 3. log user in 
+router.post('/register', userController.validateRegister)
 
 module.exports = router
