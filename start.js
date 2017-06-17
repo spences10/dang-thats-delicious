6cb1c250c86ccfef2f092a97133517c5a05300e4
spences10 <spences10apps@gmail.com>
2017-06-17 21:35:00 +0100

    Sending email with nodejs

--- start.js
+++ start.js
@@ -29,3 +29,6 @@ app.set('port', process.env.PORT || 7777)
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`)
 })
+
+// // TEMP test email
+// require('./handlers/mail')
