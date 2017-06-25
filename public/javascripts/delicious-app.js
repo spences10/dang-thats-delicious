a646c2d5e9afcfc34d479649cebd74dc9e108ad8
spences10 <spences10apps@gmail.com>
2017-06-25 21:39:19 +0100

    Push User Data to API for hearts ♥

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,6 +4,7 @@ import { $, $$ } from './modules/bling'
 import autocomplete from './modules/autocomplete'
 import typeAhead from './modules/typeAhead'
 import makeMap from './modules/map'
+import ajaxHeart from './modules/heart';
 
 // so the '$' is shorthand here for document.querySelector
 // using blingjs $$$$$!!!
@@ -12,3 +13,6 @@ autocomplete( $('#address'), $('#lat'), $('#lng'))
 typeAhead( $('.search') )
 
 makeMap( $('#map') )
+
+const heartForms = $$('form.heart')
+heartForms.on('submit', ajaxHeart)
