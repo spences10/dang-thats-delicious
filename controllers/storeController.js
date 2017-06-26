bdb9f2880594056e55878e4fbac42dae120493a4
spences10 <spences10apps@gmail.com>
2017-06-26 22:09:36 +0100

    Displaying Reviews 🙃

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -87,7 +87,9 @@ exports.updateStore = async (req, res) => {
 
 exports.getStoreBySlug = async (req, res, next) => {
   // query database for store
-  const store = await Store.findOne({ slug: req.params.slug }).populate('author')
+  const store = await Store
+    .findOne({ slug: req.params.slug })
+    .populate('author reviews')
   if(!store) return next()
   res.render('store', { store, title: store.name })
 }
--- models/Store.js
+++ models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
     ref: 'User',
     required: 'You must supply an author'
   }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 })
 
 // Define indexes
@@ -72,5 +75,12 @@ storeSchema.statics.getTagsList = function () {
     { $sort: { count: -1 } }
   ])
 }
+
+// find reviews where stores _id === reviews store
+storeSchema.virtual('reviews', {
+  ref: 'Review',
+  localField: '_id', 
+  foreignField: 'store'
+})
 
 module.exports = mongoose.model('Store', storeSchema)
