bc434e497e5bf8fc3409a2e4735018ca2f8000a4
spences10 <spences10apps@gmail.com>
2017-06-26 06:56:12 +0100

    Create Reviews Data Model :+1:

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const storeController = require('../controllers/storeController')
 const userController = require('../controllers/userController')
 const authController = require('../controllers/authController')
+const reviewController = require('../controllers/reviewController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // so requset is the information and
@@ -56,6 +57,11 @@ router.post('/account/reset/:token',
 router.get('/map/', storeController.mapPage)
 
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
+
+router.post('/reviews/:id', 
+  authController.isLoggedIn, 
+  catchErrors(reviewController.addReview)
+)
 
 /**
  * API 
